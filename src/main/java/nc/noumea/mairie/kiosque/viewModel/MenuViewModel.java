e854f9cbd5997a65c829141740c66f03022246ed
nicno85 <nicno85@ville-noumea.nc>
2014-08-26 03:50:53 +0000

    #10744 : connexion aux WS de sirh-abs-ws --> affichage du solde de l'agent

--- src/main/java/nc/noumea/mairie/kiosque/viewModel/MenuViewModel.java
+++ src/main/java/nc/noumea/mairie/kiosque/viewModel/MenuViewModel.java
@@ -7,6 +7,10 @@ import nc.noumea.mairie.kiosque.abs.dto.SoldeDto;
 import nc.noumea.mairie.ws.ISirhAbsWSConsumer;
 
 import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 
@@ -16,11 +20,46 @@ public class MenuViewModel {
 	@WireVariable
 	private ISirhAbsWSConsumer absWsConsumer;
 
+	private String action = null;
+	
+	private SoldeDto soldeCourant = new SoldeDto();
+	
+	private String matricule= null;
+
 	@Command
-	public void soldeAgent() {
+	@NotifyChange({"action","soldeCourant","matricule"})
+	public void soldeAgent(@ContextParam(ContextType.VIEW) Component window) {
 		FiltreSoldeDto filtreDto = new FiltreSoldeDto();
 		filtreDto.setDateDebut(new Date());
 		filtreDto.setDateFin(new Date());
 		SoldeDto result = absWsConsumer.getAgentSolde(9005138, filtreDto);
+		soldeCourant = result;
+		matricule = "9005138";
+		action = "visuSolde";
+
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public SoldeDto getSoldeCourant() {
+		return soldeCourant;
+	}
+
+	public void setSoldeCourant(SoldeDto soldeCourant) {
+		this.soldeCourant = soldeCourant;
+	}
+
+	public String getMatricule() {
+		return matricule;
+	}
+
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
 	}
 }
