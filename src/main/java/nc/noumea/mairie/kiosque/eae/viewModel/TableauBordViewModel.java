f29995dea8d5cea4e015f701340db918ca14fcf5
nicno85 <nicno85@ville-noumea.nc>
2014-10-16 03:50:31 +0000

    #11644 : EAE --> tableau de bord : affichage du footer

--- src/main/java/nc/noumea/mairie/kiosque/eae/viewModel/TableauBordViewModel.java
+++ src/main/java/nc/noumea/mairie/kiosque/eae/viewModel/TableauBordViewModel.java
@@ -25,8 +25,105 @@ public class TableauBordViewModel {
 	public void initTableauBord() {
 		currentUser = (ProfilAgentDto) Sessions.getCurrent().getAttribute("currentUser");
 		// on recupère les info du tableau de bord
-		List<EaeDashboardItemDto> tableau = eaeWsConsumer.getTableauBord(currentUser.getAgent().getIdAgent());
+		List<EaeDashboardItemDto> tableau = eaeWsConsumer.getTableauBord(9002990);
 		setTableauBord(tableau);
+	}
+
+	public String totalNonAffecte() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getNonAffecte();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalNonDebute() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getNonDebute();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalCree() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getCree();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalEnCours() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getEnCours();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalFinalise() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getFinalise();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalFige() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getFige();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalEae() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getNonAffecte() + dto.getNonDebute() + dto.getCree() + dto.getEnCours() + dto.getFinalise()
+					+ dto.getFige();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalNonDefini() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getNonDefini();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalMini() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getMini();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalMoy() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getMoy();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalMaxi() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getMaxi();
+		}
+		return String.valueOf(total);
+	}
+
+	public String totalChgtClasse() {
+		int total = 0;
+		for (EaeDashboardItemDto dto : getTableauBord()) {
+			total += dto.getChangClasse();
+		}
+		return String.valueOf(total);
 	}
 
 	public String concatAgent(String nom, String prenom) {
