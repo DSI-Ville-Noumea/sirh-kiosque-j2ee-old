e21886854d12117c34e1e2f0f087fa70b7f692e5
nicno85 <nicno85@ville-noumea.nc>
2015-09-06 23:18:52 +0000

    #18298 : merge Kiosque

---
+++ src/main/java/nc/noumea/mairie/ads/dto/EntiteDto.java
@@ -0,0 +1,228 @@
+package nc.noumea.mairie.ads.dto;
+
+/*
+ * #%L
+ * sirh-kiosque-j2ee
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2014 - 2015 Mairie de Nouméa
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class EntiteDto implements Comparable<EntiteDto> {
+
+	private Integer idEntite;
+	private String sigle;
+	private String label;
+	private String labelCourt;
+	private ReferenceDto typeEntite;
+	private String codeServi;
+	private List<EntiteDto> enfants;
+	private EntiteDto entiteParent;
+	private EntiteDto entiteRemplacee;
+
+	private Integer idStatut;
+	private Integer idAgentCreation;
+	private Date dateCreation;
+	private Integer idAgentModification;
+	private Date dateModification;
+	private String refDeliberationActif;
+	private Date dateDeliberationActif;
+	private String refDeliberationInactif;
+	private Date dateDeliberationInactif;
+
+	public EntiteDto() {
+		enfants = new ArrayList<>();
+	}
+
+	public Integer getIdEntite() {
+		return idEntite;
+	}
+
+	public void setIdEntite(Integer idEntite) {
+		this.idEntite = idEntite;
+	}
+
+	public String getSigle() {
+		return sigle;
+	}
+
+	public void setSigle(String sigle) {
+		this.sigle = sigle;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getLabelCourt() {
+		return labelCourt;
+	}
+
+	public void setLabelCourt(String labelCourt) {
+		this.labelCourt = labelCourt;
+	}
+
+	public ReferenceDto getTypeEntite() {
+		return typeEntite;
+	}
+
+	public void setTypeEntite(ReferenceDto typeEntite) {
+		this.typeEntite = typeEntite;
+	}
+
+	public String getCodeServi() {
+		return codeServi;
+	}
+
+	public void setCodeServi(String codeServi) {
+		this.codeServi = codeServi;
+	}
+
+	public List<EntiteDto> getEnfants() {
+		return enfants;
+	}
+
+	public void setEnfants(List<EntiteDto> enfants) {
+		this.enfants = enfants;
+	}
+
+	public EntiteDto getEntiteParent() {
+		return entiteParent;
+	}
+
+	public void setEntiteParent(EntiteDto entiteParent) {
+		this.entiteParent = entiteParent;
+	}
+
+	public EntiteDto getEntiteRemplacee() {
+		return entiteRemplacee;
+	}
+
+	public void setEntiteRemplacee(EntiteDto entiteRemplacee) {
+		this.entiteRemplacee = entiteRemplacee;
+	}
+
+	public Integer getIdStatut() {
+		return idStatut;
+	}
+
+	public void setIdStatut(Integer idStatut) {
+		this.idStatut = idStatut;
+	}
+
+	public Integer getIdAgentCreation() {
+		return idAgentCreation;
+	}
+
+	public void setIdAgentCreation(Integer idAgentCreation) {
+		this.idAgentCreation = idAgentCreation;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	public Integer getIdAgentModification() {
+		return idAgentModification;
+	}
+
+	public void setIdAgentModification(Integer idAgentModification) {
+		this.idAgentModification = idAgentModification;
+	}
+
+	public Date getDateModification() {
+		return dateModification;
+	}
+
+	public void setDateModification(Date dateModification) {
+		this.dateModification = dateModification;
+	}
+
+	public String getRefDeliberationActif() {
+		return refDeliberationActif;
+	}
+
+	public void setRefDeliberationActif(String refDeliberationActif) {
+		this.refDeliberationActif = refDeliberationActif;
+	}
+
+	public Date getDateDeliberationActif() {
+		return dateDeliberationActif;
+	}
+
+	public void setDateDeliberationActif(Date dateDeliberationActif) {
+		this.dateDeliberationActif = dateDeliberationActif;
+	}
+
+	public String getRefDeliberationInactif() {
+		return refDeliberationInactif;
+	}
+
+	public void setRefDeliberationInactif(String refDeliberationInactif) {
+		this.refDeliberationInactif = refDeliberationInactif;
+	}
+
+	public Date getDateDeliberationInactif() {
+		return dateDeliberationInactif;
+	}
+
+	public void setDateDeliberationInactif(Date dateDeliberationInactif) {
+		this.dateDeliberationInactif = dateDeliberationInactif;
+	}
+
+	@Override
+	public int compareTo(EntiteDto arg0) {
+		
+		if(null == this.label
+				|| null == arg0.label) {
+			return 0;
+		}
+		
+		return this.label.compareTo(arg0.label);
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		
+		if(null == obj
+				|| null == ((EntiteDto) obj).getIdEntite()
+				|| null == idEntite) {
+			return false;
+		}
+		
+		return idEntite.equals(((EntiteDto) obj).getIdEntite());
+	}
+	
+}
---
+++ src/main/java/nc/noumea/mairie/ads/dto/ReferenceDto.java
@@ -0,0 +1,49 @@
+package nc.noumea.mairie.ads.dto;
+
+/*
+ * #%L
+ * sirh-kiosque-j2ee
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2014 - 2015 Mairie de Nouméa
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+
+public class ReferenceDto {
+
+	public Integer id;
+	public String label;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+}
