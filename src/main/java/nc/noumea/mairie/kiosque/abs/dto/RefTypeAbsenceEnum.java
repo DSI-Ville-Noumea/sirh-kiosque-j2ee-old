b4c15b5eb772adf6de75527f5763391ecbc0d76c
nicno85 <nicno85@ville-noumea.nc>
2015-09-30 23:55:18 +0000

    #18742 : Création : CONGE EXCEPTIONNEL MEMBRE DU BUREAU de l'amicale de la ville

--- src/main/java/nc/noumea/mairie/kiosque/abs/dto/RefTypeAbsenceEnum.java
+++ src/main/java/nc/noumea/mairie/kiosque/abs/dto/RefTypeAbsenceEnum.java
@@ -28,7 +28,7 @@ package nc.noumea.mairie.kiosque.abs.dto;
 public enum RefTypeAbsenceEnum {
 
 	CONGE_ANNUEL(1), REPOS_COMP(2), RECUP(3), MALADIES(6), ASA_A48(7), ASA_A54(8), ASA_A55(9), ASA_A52(10), ASA_A53(
-			11), ASA_A49(12), ASA_A50(13), ASA_AMICALE(67);
+			11), ASA_A49(12), ASA_A50(13), ASA_AMICALE(69);
 
 	private int type;
 
@@ -68,7 +68,7 @@ public enum RefTypeAbsenceEnum {
 				return ASA_A49;
 			case 13:
 				return ASA_A50;
-			case 67:
+			case 69:
 				return ASA_AMICALE;
 			default:
 				return null;
