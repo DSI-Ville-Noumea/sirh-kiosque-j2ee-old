e61c20532718fac27527890e0ed550542c901ae3
rebjo84 <rebjo84@ville-noumea.nc>
2016-03-28 22:38:39 +0000

    #29868 Ajouter un champ commentaire aux pièce jointes

--- src/main/java/nc/noumea/mairie/kiosque/abs/dto/PieceJointeDto.java
+++ src/main/java/nc/noumea/mairie/kiosque/abs/dto/PieceJointeDto.java
@@ -1,6 +1,37 @@
 package nc.noumea.mairie.kiosque.abs.dto;
 
+/*
+ * #%L
+ * sirh-kiosque-j2ee
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2014 - 2016 Mairie de Nouméa
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import java.io.Serializable;
+import java.util.Date;
+
+import nc.noumea.mairie.kiosque.dto.JsonDateDeserializer;
+import nc.noumea.mairie.kiosque.dto.JsonDateSerializer;
+
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 public class PieceJointeDto implements Serializable {
 
@@ -16,6 +47,10 @@ public class PieceJointeDto implements Serializable {
 	private String titre;
 	private String urlFromAlfresco;
 	private String nodeRefAlfresco;
+	private String commentaire;
+	@JsonSerialize(using = JsonDateSerializer.class)
+	@JsonDeserialize(using = JsonDateDeserializer.class)
+	private Date dateModification;
 	
 	
 	public Integer getIdPieceJointe() {
@@ -53,6 +88,18 @@ public class PieceJointeDto implements Serializable {
 	}
 	public void setNodeRefAlfresco(String nodeRefAlfresco) {
 		this.nodeRefAlfresco = nodeRefAlfresco;
+	}
+	public String getCommentaire() {
+		return commentaire;
+	}
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+	public Date getDateModification() {
+		return dateModification;
+	}
+	public void setDateModification(Date dateModification) {
+		this.dateModification = dateModification;
 	}
 
 }
