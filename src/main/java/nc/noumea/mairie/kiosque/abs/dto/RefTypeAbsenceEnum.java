65d9fb3334a957f3acaa2f3f562cd368b7a87d6e
nicno85 <nicno85@ville-noumea.nc>
2017-04-25 04:26:33 +0000

    Supression typeAbsence inutile

--- src/main/java/nc/noumea/mairie/kiosque/abs/dto/RefTypeAbsenceEnum.java
+++ src/main/java/nc/noumea/mairie/kiosque/abs/dto/RefTypeAbsenceEnum.java
@@ -27,7 +27,7 @@ package nc.noumea.mairie.kiosque.abs.dto;
 
 public enum RefTypeAbsenceEnum {
 
-	CONGE_ANNUEL(1), REPOS_COMP(2), RECUP(3), MALADIES(6), ASA_A48(7), ASA_A54(8), ASA_A55(9), ASA_A52(10), ASA_A53(
+	CONGE_ANNUEL(1), REPOS_COMP(2), RECUP(3), ASA_A48(7), ASA_A54(8), ASA_A55(9), ASA_A52(10), ASA_A53(
 			11), ASA_A49(12), ASA_A50(13), ASA_AMICALE(69), ACCIDENT_TRAVAIL(77);
 
 	private int type;
@@ -52,8 +52,6 @@ public enum RefTypeAbsenceEnum {
 				return REPOS_COMP;
 			case 3:
 				return RECUP;
-			case 6:
-				return MALADIES;
 			case 7:
 				return ASA_A48;
 			case 8:
@@ -89,8 +87,6 @@ public enum RefTypeAbsenceEnum {
 				return "Repos compensateur";
 			case 3:
 				return "Récupérations";
-			case 6:
-				return "Maladies";
 			case 7:
 				return "Réunion des membres du bureau directeur";
 			case 8:
